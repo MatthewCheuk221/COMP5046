4f44e55f62df01495430aca831f906cce66e5cd3
MatthewCheuk221 <80976897+MatthewCheuk221@users.noreply.github.com>
2025-06-18 22:16:23 +1000

    Update q5_metrics.py

--- COMP5046_Assignment_2/q5_metrics.py
+++ COMP5046_Assignment_2/q5_metrics.py
@@ -76,7 +76,6 @@ def calculate_macro_f1(confusion_matrix, labels):
     """
     
     f_scores = []
-    
     precision = calculate_precision(confusion_matrix, labels)
     recall = calculate_recall(confusion_matrix, labels)
     
